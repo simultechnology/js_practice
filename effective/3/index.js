00bf79a364b5db52583d922173ed6965d21c4180
takatsugu ishikawa <tissi0708@gmail.com>
2014-07-25 08:46:57 +0900

    add average function and test case framework

--- effective/3/index.js
+++ effective/3/index.js
@@ -1,7 +1,16 @@
 
 function averageOfArray(a) {
+    "use strict";
     for (var i = 0, sum = 0, n = a.length; i < n; i++) {
         sum += a[i];
     }
-    return sum / a;
+    return sum / n;
+}
+
+function average() {
+    "use strict";
+    for (var i = 0, sum = 0, n = arguments.length; i < n; i++) {
+        sum += arguments[i];
+    }
+    return sum / n;
 }
