4e5c303c9d60ff76bd223dadba82d5f02ea0f911
t_ishikawa <tissi0708@gmail.com>
2016-04-05 10:20:49 +0900

    add vagrant files

--- frontend/grunt/webdb77/src/sample01.js
+++
@@ -1,6 +0,0 @@
-var hello = function (user) {
-    if (user.age) {
-        user.job = 'programmer';
-    }
-    console.log('Hello, ' + user.name);
-};
--- frontend/grunt/webdb77/src/sample02.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    var user = {
-        'name': "tissi",
-        'age': 37,
-        'home': 'Tokyo'
-    };
-    console.log(user);
-    hello(user);
-}());
